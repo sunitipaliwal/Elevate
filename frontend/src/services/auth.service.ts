df5eed56b0d33600a21aab6335908a0f25bdb999
sunitipaliwal <paliwalsuniti99@gmail.com>
2026-03-18 09:48:38 +0530

    production

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,15 +1,17 @@
-import axiosInstance from "@/lib/axios"
-import { User } from "@/types/user.types"
+import axiosInstance from "@/lib/axios";
+import { User } from "@/types/user.types";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getCurrentUser = async (): Promise<User> => {
-  const res = await axiosInstance.get("/auth/me")
-  return res.data
-}
+  const res = await axiosInstance.get("/auth/me");
+  return res.data;
+};
 
 export const logoutUser = async () => {
-  await axiosInstance.post("/auth/logout")
-}
+  await axiosInstance.post("/auth/logout");
+};
 
 export const googleLogin = () => {
-  window.location.href = "http://localhost:5000/api/auth/google"
-}
+  window.location.href = `${API_URL}/auth/google`;
+};
